4aa1c1832e909bb089b582c354c78999a8eec390
Yosa Rama <yosamelody07@gmail.com>
2022-07-01 19:11:42 +0700

    Finish Boost Dynamic Table

---
+++ components/loader.js
@@ -0,0 +1,25 @@
+function Loader() {
+  return (
+    <>
+      <div className="loader-container">
+        <div className="lds-default">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <p className="loader-desc">just for a good UI</p>
+      </div>
+    </>
+  );
+}
+
+export default Loader;
